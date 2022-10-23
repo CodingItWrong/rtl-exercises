c0182496793f21a1776db130526b71580d406589
Josh Justice <me@codingitwrong.com>
2022-10-23 18:43:39 +0100

    Exercise 1 solution

--- src/MovieRow.spec.js
+++ src/MovieRow.spec.js
@@ -2,10 +2,24 @@ import {render, screen} from '@testing-library/react';
 import MovieRow from './MovieRow';
 
 describe('MovieRow', () => {
-  it('NAME AND ADD YOUR TEST HERE', () => {
-    // render(<MovieRow ... />);
-    // expect(...)
+  it('displays the name', () => {
+    const movie = {title: 'Spider-Man: No Way Home'};
+    render(<MovieRow movie={movie} />);
+
+    expect(screen.getByText(movie.title)).toBeVisible();
   });
 
-  // add more tests as necessary
+  it('renders a new icon when the movie was added today', () => {
+    const movie = {addedToday: true};
+    render(<MovieRow movie={movie} />);
+
+    expect(screen.getByTitle('Added Today')).toBeVisible();
+  });
+
+  it('does not render a new icon when the movie was not added today', () => {
+    const movie = {addedToday: false};
+    render(<MovieRow movie={movie} />);
+
+    expect(screen.queryByTitle('Added Today')).not.toBeInTheDocument();
+  });
 });

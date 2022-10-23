98d99160d04efba2613542c2a3f162162a092a16
Josh Justice <me@codingitwrong.com>
2022-10-23 18:43:39 +0100

    Exercise 1 solution

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -4,7 +4,9 @@ export default function MovieRow({movie}) {
   return (
     <li>
       <span>{movie.title}</span>
-      {movie.addedToday && <New fill="orange" width="30" height="30" />}
+      {movie.addedToday && (
+        <New fill="orange" width="30" height="30" title="Added Today" />
+      )}
     </li>
   );
 }
